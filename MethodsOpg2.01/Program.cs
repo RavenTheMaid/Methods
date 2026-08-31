01fe45e61cd0977358e33864ae35429216cf769b
RavenTheMaid <62701@edu.campusvejle.dk>
2026-08-31 10:44:33 +0200

    blob

---
+++ MethodsOpg2.01/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MethodsOpg2._01
+{
+    internal class Program
+    {
+        static int[] Generate(int numbersToGenerate)
+        {
+            Random numberGenerator = new Random();
+            int[] array = new int[numbersToGenerate];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int generatedNumber = numberGenerator.Next(1, 100);
+                array[i] = generatedNumber;
+            }
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] epicArray = Generate(50);
+            for (int i = 0; i < epicArray.Length; i++)
+            {
+                Console.WriteLine($"{i+ 1}: {epicArray[i]}");
+            }
+        }
+    }
+}
---
+++ MethodsOpg3.01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace MethodsOpg3._01
+{
+    internal class Program
+    {
+        static string ToString(string randomName)
+        {
+            string stringName = "";
+            char[] array;
+            array = randomName.ToCharArray(0, randomName.Length);
+            foreach (char character in array)
+            {
+                stringName += character + ", ";
+            }
+            return stringName;
+        }
+
+        //static string Generate()
+        //{
+
+        //}
+
+        static void Main(string[] args)
+        {
+
+            
+            string anotherStringName = ToString("blimbleblomble");
+            
+
+        }
+    }
+}
